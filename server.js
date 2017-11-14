56280040b9fadb11c09d4632d4e2ae73a2176f30
danny perez <perez6736@gmail.com>
2017-11-14 00:07:11 -0500

    set up the local server and basic routing

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 // the basic express server start up goes here. 
 
 var express = require('express');
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routing 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+  });
+
 
 
 app.listen(PORT);
