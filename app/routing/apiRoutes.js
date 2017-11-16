51c8fdaf0354aafcaabe8efe8b1dc472cbf3f336
danny perez <perez6736@gmail.com>
2017-11-15 23:36:08 -0500

    post the form to the server and save it in the friends.js file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,5 +12,14 @@ module.exports = function(app) {
       // write a post request of the match. 
       // logic comparing the users values to all the other friends go here. 
       // after finding a suitable friend respond with that friend. 
+    app.post("/api/routes", function(req, res){
+
+        for(i=0; i<req.body.scores.length; i++){
+            req.body.scores[i] = parseInt(req.body.scores[i]);
+        }
+        
+        friends.push(req.body);
+        
+    });
 };
   
